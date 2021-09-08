10a5f66d2c7906877e5009f1a7e5e952a8ffbe8a
twinh <twinhuang@qq.com>
2021-09-08 15:20:21 +0800

    test(install): 测试安装流程

---
+++ pages/admin/install/index.test.js
@@ -0,0 +1,63 @@
+import Index from './index';
+import $, {Ret} from 'miaoxing';
+import {fireEvent, render, waitFor} from '@testing-library/react';
+import {bootstrap, resetUrl, setUrl} from '@mxjs/test';
+import {app} from '@mxjs/app';
+
+bootstrap();
+
+describe('install', () => {
+  beforeEach(() => {
+    setUrl('admin/install');
+    app.page = {
+      collection: 'admin/install',
+      index: true,
+    };
+  });
+
+  afterEach(() => {
+    resetUrl();
+    app.page = {};
+  });
+
+  test('install', async () => {
+    $.http = jest.fn()
+      .mockResolvedValueOnce({
+        ret: Ret.suc({
+          data: {
+            installRet: Ret.suc({
+              data: [
+                Ret.suc('check suc 1'),
+                Ret.suc('check suc 2'),
+              ],
+            }),
+            license: 'xxx',
+          },
+        }),
+      })
+      // 测试 rewrite
+      .mockResolvedValueOnce({
+        ret: Ret.suc(),
+      })
+      .mockResolvedValueOnce({
+        ret: Ret.suc(),
+      });
+
+    const result = render(<Index/>);
+
+    await result.findByText('check suc 1');
+
+    fireEvent.click(result.getByText('进入安装'));
+    fireEvent.change(result.getByLabelText('数据库密码'), {target: {value: 'password'}});
+    fireEvent.change(result.getByLabelText('管理员密码'), {target: {value: '123456'}});
+    fireEvent.click(result.getByLabelText('安装演示数据'));
+    fireEvent.click(result.getByText('我已阅读并同意'));
+    fireEvent.click(result.getByText('安 装'));
+
+    await waitFor(() => {
+      expect($.http).toBeCalledTimes(3);
+    });
+
+    expect($.http).toMatchSnapshot();
+  });
+});
