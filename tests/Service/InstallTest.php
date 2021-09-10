5d4fc710f4a0a02664afba00c7fdfc098eda4870
twinh <twinhuang@qq.com>
2021-09-10 15:02:46 +0800

    test(install): 测试 `checkDirs`

--- tests/Service/InstallTest.php
+++ tests/Service/InstallTest.php
@@ -68,4 +68,49 @@ final class InstallTest extends BaseTestCase
         $ret = Install::checkInstall();
         $this->assertRetErr($ret, '程序已安装过，如需重新安装，请手动删除：storage/install.lock.php');
     }
+
+    public function testCheckDirsErr()
+    {
+        $install = $this->getServiceMock(Install::class, [
+            'isInstalled',
+        ]);
+
+        $install->expects($this->once())
+            ->method('isInstalled')
+            ->willReturn(false);
+
+        $install->setOption('checkDirs', [
+            'invalid:/path',
+        ]);
+
+        $ret = Install::checkInstall();
+
+        $this->assertRetErr($ret, '目录 invalid:/path 不可写');
+    }
+
+    public function testCheckDirsSuc()
+    {
+        $dir = 'public/uploads';
+        if (!is_dir($dir) && !@mkdir($dir, 0755, true)) {
+            $message = sprintf('Failed to create directory "%s"', $dir);
+            ($e = error_get_last()) && $message .= ': ' . $e['message'];
+            throw new \RuntimeException($message);
+        }
+
+        $install = $this->getServiceMock(Install::class, [
+            'isInstalled',
+            'checkExts',
+        ]);
+
+        $install->expects($this->once())
+            ->method('isInstalled')
+            ->willReturn(false);
+
+        $install->expects($this->once())
+            ->method('checkExts')
+            ->willReturn([]);
+
+        $ret = Install::checkInstall();
+        $this->assertRetSuc($ret);
+    }
 }
