8500837f070fb2fabb65caea943e489889ae44d8
twinh <twinhuang@qq.com>
2021-09-10 10:24:22 +0800

    test(install): 测试安装成功和失败

--- tests/Service/InstallTest.php
+++ tests/Service/InstallTest.php
@@ -28,4 +28,44 @@ final class InstallTest extends BaseTestCase
             unlink($file);
         }
     }
+
+    public function testCheckInstallSuc()
+    {
+        $install = $this->getServiceMock(Install::class, [
+            'isInstalled',
+            'checkStorageDir',
+            'checkExts',
+        ]);
+
+        $install->expects($this->once())
+            ->method('isInstalled')
+            ->willReturn(false);
+
+        $install->expects($this->once())
+            ->method('checkStorageDir')
+            ->willReturn(suc());
+
+        $install->expects($this->once())
+            ->method('checkExts')
+            ->willReturn([]);
+
+        $ret = Install::checkInstall();
+
+        $this->assertRetSuc($ret);
+        $this->assertRetSuc($ret->getData()[0]);
+    }
+
+    public function testCheckInstallErr()
+    {
+        $install = $this->getServiceMock(Install::class, [
+            'isInstalled',
+        ]);
+
+        $install->expects($this->once())
+            ->method('isInstalled')
+            ->willReturn(true);
+
+        $ret = Install::checkInstall();
+        $this->assertRetErr($ret, '程序已安装过，如需重新安装，请手动删除：storage/install.lock.php');
+    }
 }
