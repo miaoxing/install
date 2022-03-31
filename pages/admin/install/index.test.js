30f84739ba43fa9e51d106987874269e865db907
twinh <twinhuang@qq.com>
2022-03-31 18:22:46 +0800

    test(install): 如果是首次检查，并且可以安装，则直接显示安装表单

--- pages/admin/install/index.test.js
+++ pages/admin/install/index.test.js
@@ -45,9 +45,9 @@ describe('install', () => {
 
     const result = render(<Index/>);
 
-    await result.findByText('check suc 1');
+    // 检查成功后自动进入安装页面
+    await result.findByText('数据库密码');
 
-    fireEvent.click(result.getByText('进入安装'));
     fireEvent.change(result.getByLabelText('数据库密码'), {target: {value: 'password'}});
     fireEvent.change(result.getByLabelText('管理员密码'), {target: {value: '123456'}});
     fireEvent.click(result.getByLabelText('安装演示数据'));
@@ -166,8 +166,9 @@ describe('install', () => {
 
     const result = render(<Index/>);
 
-    const btn = await result.findByText('进入安装');
-    fireEvent.click(btn);
+    // 检查成功后自动进入安装页面
+    await result.findByText('数据库密码');
+
     fireEvent.change(result.getByLabelText('数据库密码'), {target: {value: 'password'}});
     fireEvent.change(result.getByLabelText('管理员密码'), {target: {value: '123456'}});
     fireEvent.click(result.getByText('我已阅读并同意'));
